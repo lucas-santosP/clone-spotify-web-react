a15c65dfc92ab7659fea82e69baeb3b42f0208d1
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-08-10 13:56:02 -0300

    Updates

---
+++ precache-manifest.4e162fbf1b0f921808e1c451a99c340e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43747fa66f2b4c28d523e9042a85f69a",
+    "url": "/clone-spotify-web-react/index.html"
+  },
+  {
+    "revision": "3d7e32e397625d822f0b",
+    "url": "/clone-spotify-web-react/static/css/main.bf04b32b.chunk.css"
+  },
+  {
+    "revision": "6060abffc29c4335c685",
+    "url": "/clone-spotify-web-react/static/js/2.81f7e05d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone-spotify-web-react/static/js/2.81f7e05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7e32e397625d822f0b",
+    "url": "/clone-spotify-web-react/static/js/main.266f8f2e.chunk.js"
+  },
+  {
+    "revision": "d112bf4566d07d0c0dea",
+    "url": "/clone-spotify-web-react/static/js/runtime-main.c0ff7ef8.js"
+  },
+  {
+    "revision": "bcb601f6fb006521da5d9312bfaa4191",
+    "url": "/clone-spotify-web-react/static/media/chevron-left.bcb601f6.svg"
+  },
+  {
+    "revision": "0d0fcc88b6de86c2b6028675ed116fe8",
+    "url": "/clone-spotify-web-react/static/media/chevron-right.0d0fcc88.svg"
+  },
+  {
+    "revision": "11ee6d781666d1e5f06c89c9bf769f3a",
+    "url": "/clone-spotify-web-react/static/media/cover-example.11ee6d78.jfif"
+  },
+  {
+    "revision": "765c55c5222caa1832c5e5966e8f7e90",
+    "url": "/clone-spotify-web-react/static/media/user.765c55c5.jpg"
+  }
+]);
