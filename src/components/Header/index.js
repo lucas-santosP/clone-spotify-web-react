a434cff3a984341de0c56a61c7fa0ff5f442d188
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-08-24 23:19:58 -0300

    Adiciona funcionalidades nos botoes goBack e goFoward no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./styles.scss";
-
+import { useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
@@ -9,12 +9,21 @@ import {
 import ButtonUser from "./ButtonUser";
 
 const MainHeader = () => {
+  const history = useHistory();
   const [isScrolled, setIsScrolled] = useState(false);
 
   function updateIsScrolled() {
     if (document.querySelector(".app-main").scrollTop >= 50)
       setIsScrolled(true);
     else setIsScrolled(false);
+  }
+
+  function goBack() {
+    history.goBack();
+  }
+
+  function goFoward() {
+    history.goForward();
   }
 
   useEffect(() => {
@@ -25,10 +34,10 @@ const MainHeader = () => {
 
   return (
     <header className={`main-header ${isScrolled ? "header-scrolled" : ""}`}>
-      <button className="btn-arrow">
+      <button className="btn-arrow" onClick={goBack}>
         <FontAwesomeIcon icon={faChevronLeft} color={"#fff"} />
       </button>
-      <button className="btn-arrow">
+      <button className="btn-arrow" onClick={goFoward}>
         <FontAwesomeIcon icon={faChevronRight} color={"#fff"} />
       </button>
 
