46443e213b588b0b967364ee0651d08d1fa51452
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-09-20 14:29:41 -0300

    Refatoracao simplificando a requisicao de albums da api do Lastfm

--- src/common/services/modules/album.js
+++ src/common/services/modules/album.js
@@ -3,19 +3,11 @@ import api from "../api";
 export default {
   async get(limit = 51) {
     try {
-      const response = await api.get(
-        `/?method=library.getartists&api_key=${process.env.REACT_APP_LASTFM_API_KEY}&user=Lukas_Maxson&format=json&limit=${limit}`
+      const responde = await api.get(
+        `?method=user.gettopalbums&user=Lukas_Maxson&api_key=${process.env.REACT_APP_LASTFM_API_KEY}&format=json&limit=${limit}`
       );
-      const artists = response.data.artists.artist;
 
-      const albums = [];
-      for (let i = 0; i < artists.length; i++) {
-        const responde = await api.get(
-          `/?method=artist.gettopalbums&artist=${artists[i].name}&api_key=${process.env.REACT_APP_LASTFM_API_KEY}&format=json&limit=1`
-        );
-        await albums.push(...responde.data.topalbums.album);
-      }
-      return albums;
+      return responde.data.topalbums.album;
     } catch (error) {
       console.log(error);
     }
