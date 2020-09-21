74b558291b9618bf108d2a2612d2950e5200b8d1
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-09-21 13:33:55 -0300

    Exibe podcasts na tela YourLibrary

--- src/views/YourLibrary/index.js
+++ src/views/YourLibrary/index.js
@@ -2,19 +2,41 @@ import React from "react";
 import "./styles.scss";
 
 import { useStore } from "../../store";
+import { normalizeImageUrl } from "../../common/utils";
 import Card from "../../components/ui/Card";
 import SectionHeader from "../../components/ui/SectionHeader";
 
 const YourLibrary = () => {
-  const { categories } = useStore();
+  const { categories, albums, podcasts } = useStore();
 
   return (
     <div className="your-library-container">
       <SectionHeader title={categories.current} />
+
       <div className="grid-cards">
-        <Card className="grid-item" title={"Example"} subtitle={"Example"} />
-        <Card className="grid-item" title={"Example"} subtitle={"Example"} />
-        <Card className="grid-item" title={"Example"} subtitle={"Example"} />
+        {categories.current === "Álbuns"
+          ? albums.map((album) => (
+              <Card
+                className="grid-item"
+                key={album.mbid + album.name}
+                title={album.name}
+                subtitle={album.artist.name}
+                image={normalizeImageUrl(album.image)}
+              />
+            ))
+          : null}
+
+        {categories.current === "Podcasts"
+          ? podcasts.map((podcast) => (
+              <Card
+                className="grid-item"
+                key={podcast.mbid + podcast.name}
+                title={podcast.name}
+                subtitle={podcast.name}
+                image={normalizeImageUrl(podcast.image)}
+              />
+            ))
+          : null}
       </div>
     </div>
   );
