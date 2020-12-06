dae00384553d5b54bc4f9741c6764cc7b0c3fa7b
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-05 22:16:22 -0300

    Adiciona api key de producao no redirect do login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -5,7 +5,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
-  const redirectUri = "http://localhost:3000/clone-spotify-web-react";
+  const redirectUri =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/clone-spotify-web-react"
+      : "https://lucas-santosp.github.io/clone-spotify-web-react";
   const scopes =
     "user-library-read user-follow-read user-top-read playlist-read-collaborative";
 
@@ -19,7 +22,9 @@ const Login = () => {
         <a
           className="btn-link"
           href={`https://accounts.spotify.com/authorize?client_id=${
-            process.env.REACT_APP_SPOTIFY_API_KEY
+            process.env.NODE_ENV === "development"
+              ? process.env.REACT_APP_SPOTIFY_API_KEY
+              : process.env.REACT_APP_SPOTIFY_API_KEY_PROD
           }&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
             scopes
           )}&response_type=token`}
