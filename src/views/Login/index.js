dfbe891878e52511abe8d4f269a8c54751a4cfaa
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-17 21:22:27 -0300

    feat(Login): Adiciona subtitulo de aviso sobre o login necessario

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -9,26 +9,28 @@ const Login = () => {
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/clone-spotify-web-react"
       : "https://lucas-santosp.github.io/clone-spotify-web-react";
-  const scopes =
-    "user-library-read user-follow-read user-top-read playlist-read-collaborative playlist-read-private";
+  const scopes = `user-library-read user-follow-read user-top-read playlist-read-collaborative playlist-read-private`;
 
   function redirectToLogin() {
     window.location = `https://accounts.spotify.com/authorize?client_id=${
       process.env.NODE_ENV === "development"
         ? process.env.REACT_APP_SPOTIFY_API_KEY
         : process.env.REACT_APP_SPOTIFY_API_KEY_PROD
-    }&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
-      scopes
-    )}&response_type=token`;
+    }&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes)}&response_type=token`;
   }
 
   return (
     <div className="login-container">
       <div className="overlay"></div>
       <div className="login-modal">
-        <p className="text">
-          Spotify login is required, after finishing you will be redirected back.
-        </p>
+        <h1 className="title">
+          Spotify login is required to continue, after finishing you will be redirected back.
+        </h1>
+        <h2 className="subtitle">
+          Note that, all data accessed by this system are read-only and non-private.
+          <b> We will not have access to your email.</b> You will be able to check all the
+          information accessed before accepting it.
+        </h2>
         <button className="btn-link" onClick={redirectToLogin}>
           Go to Spotify login
           <FontAwesomeIcon icon={faSignInAlt} />
